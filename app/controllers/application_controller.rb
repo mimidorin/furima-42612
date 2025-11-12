bb8038036d8e8588261ee11f40909f1ee20d219c
mimidorin <greenfieldmidoriw@gmail.com>
2025-11-13 05:37:54 +0900

    Add HTTP Basic Auth in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(username, ENV.fetch("BASIC_AUTH_USER", ""))
+        && ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch("BASIC_AUTH_PASSWORD", ""))
+    end
+  end
 end
